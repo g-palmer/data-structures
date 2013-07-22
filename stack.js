aae7bda918b26054a96c8a32f4fa795c70bba38c
Greg Palmer <gpalmer323@gmail.com>
2013-07-22 12:32:27 -0700

    get min value of stack

--- stack.js
+++ stack.js
@@ -1,15 +1,30 @@
 var Stack = function() {
   var _storage = [];
+  var _minStorage = [];
   var stack = {
+
     push: function(value) {
+      if (!_minStorage.length || value <= _minStorage[_minStorage.length-1]) {
+        _minStorage.push(value);
+      }
       return _storage.push(value);
     },
+
     pop: function() {
+      if (_storage[_storage.length - 1] === _minStorage[_minStorage.length - 1]) {
+        _minStorage.pop();
+      }
       return _storage.length ? _storage.pop() : alert('Stack is empty');
     },
+
     length: function() {
       return _storage.length;
+    },
+
+    getMinValue: function() {
+      return _minStorage.length ? _minStorage[_minStorage.length-1] : alert('Stack is empty');
     }
+
   };
   return stack;
 };
