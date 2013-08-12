2c5ac3c7ee7d7f1416b9c9bd8080d5968c4240c7
Greg Palmer <gpalmer323@gmail.com>
2013-08-11 22:29:39 -0700

    begin prefix tree with insert spec

---
+++ src/prefixTree/prefixTreeSpec.js
@@ -0,0 +1,21 @@
+describe("PrefixTree", function() {
+  var prefixTree;
+
+  beforeEach(function() {
+    prefixTree = new PrefixTree();
+  });
+
+  it("should have methods named 'insert'", function() {
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
+  });
+
+  describe('insert', function() {
+
+    it('should correctly insert a given word string', function() {
+      prefixTree.insert('ball');
+      expect(prefixTree.children[0].letter).toEqual('b');
+    });
+
+  });
+
+});
