783c48f367d43d2b620ed449b2aa0b43f2475619
Greg Palmer <gpalmer323@gmail.com>
2013-08-11 22:46:59 -0700

    test for duplicate prefix entry

--- src/prefixTree/prefixTreeSpec.js
+++ src/prefixTree/prefixTreeSpec.js
@@ -11,14 +11,22 @@ describe("PrefixTree", function() {
 
   describe('insert', function() {
 
-    it('should correctly insert a given word string', function() {
+    it('should correctly insert a given word', function() {
       prefixTree.insert('ball');
+      prefixTree.insert('dog');
       expect(prefixTree.children[0].letter).toEqual('b');
+      expect(prefixTree.children[1].letter).toEqual('d');
     });
 
     it('should correctly identify the end of a full word', function() {
       prefixTree.insert('be');
       expect(prefixTree.children[0].children[0].fullWord).toBe(true);
+    });
+
+    it('should not create new child nodes if prefix already exists', function() {
+      prefixTree.insert('ball');
+      prefixTree.insert('boy');
+      expect(prefixTree.children.length).toEqual(1);
     });
 
   });
